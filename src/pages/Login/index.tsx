5d6f3e2fa6ad6f96506aef8ea35859af94a11b65
Kelvy <kelvy0064@gmail.com>
2020-09-16 10:31:25 -0300

    feat: added login screen

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import Header from '../../components/Header';
+import React,  { Component } from 'react'
+import Header from '../../components/Header'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
 
-const Login: React.FC = () => {
+//const Login: React.FC = () => {
+export class Login extends Component {
+  constructor (props: any) {
+    super(props)
+    this.state = {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+  render() {
   return (
     <>
       <Header/>
-      <h2>login . . .</h2>
+      <section className="login">
+        <section className="login-fields">
+        <TextField
+          id = "email"
+          label = "Email"
+          variant = "outlined" />
+        <TextField
+          type = "password"
+          id = "outlined-basic"
+          label = "Senha"
+          variant = "outlined" />
+          <a href="#" className="recover-password">
+            Esqueci minha senha
+          </a>
+          <Button
+            className="btn-login"
+            variant="outlined"
+            color="primary">Logar</Button>
+        </section>
+      </section>
     </>
   );
+  }
 }
 
-export default Login;
+export default Login
